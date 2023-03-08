52d99121b6d967b47b9a411b6dac5c17dfed0028
Patrick McDaniel <dfwjpm@outlook.com>
2023-03-08 17:21:12 -0600

    add reviews

--- resolvers/Product.js
+++ resolvers/Product.js
@@ -4,4 +4,9 @@ exports.Product = {
     const categoryId = parent.categoryId;
     return categories.find((category) => category.id === categoryId);
   },
+  reviews: (parent, args, context) => {
+    const reviews = context.reviews;
+    const id = parent.id;
+    return reviews.filter((review) => review.productId === id);
+  },
 };
